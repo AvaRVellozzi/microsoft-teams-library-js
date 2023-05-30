5fa9bed950a5fb161b8e931f0e784facedc89109
Ava Vellozzi <t-avellozzi@microsoft.com>
2023-05-30 11:50:33 -0700

    initial commit, sample app

---
+++ AvaVellozzi_SampleApp/src/components/sample/Welcome.tsx
@@ -0,0 +1,125 @@
+import { useContext, useState } from "react";
+import * as microsoftTeams from "@microsoft/teams-js";
+
+import {
+  Image,
+  TabList,
+  Tab,
+  SelectTabEvent,
+  SelectTabData,
+  TabValue,
+} from "@fluentui/react-components";
+import "./Welcome.css";
+import { EditCode } from "./EditCode";
+import { app } from "@microsoft/teams-js";
+import { AzureFunctions } from "./AzureFunctions";
+import { Graph } from "./Graph";
+import { CurrentUser } from "./CurrentUser";
+import { useData } from "@microsoft/teamsfx-react";
+import { Deploy } from "./Deploy";
+import { Publish } from "./Publish";
+import { TeamsFxContext } from "../Context";
+
+export function Welcome(props: { showFunction?: boolean; environment?: string }) {
+  const { showFunction, environment } = {
+    showFunction: true,
+    environment: window.location.hostname === "localhost" ? "local" : "azure",
+    ...props,
+  };
+  const friendlyEnvironmentName =
+    {
+      local: "local environment",
+      azure: "Azure environment",
+    }[environment] || "local environment";
+
+  const [selectedValue, setSelectedValue] = useState<TabValue>("local");
+
+  const onTabSelect = (event: SelectTabEvent, data: SelectTabData) => {
+    setSelectedValue(data.value);
+  };
+  const { teamsUserCredential } = useContext(TeamsFxContext);
+  const { loading, data, error } = useData(async () => {
+    if (teamsUserCredential) {
+      const userInfo = await teamsUserCredential.getUserInfo();
+      return userInfo;
+    }
+  });
+  const userName = loading || error ? "" : data!.displayName;
+  const hubName = useData(async () => {
+    await app.initialize();
+    const context = await app.getContext();
+    return context.app.host.name;
+  })?.data;
+  return (
+    <div className="welcome page">
+      <div className="narrow page-padding">
+        <Image src="hello.png" />
+        <h1 className="center">Congratulations{userName ? ", " + userName : ""}!</h1>
+        {hubName && <p className="center">Your app is running in {hubName}</p>}
+        <p className="center">Your app is running in your {friendlyEnvironmentName}</p>
+
+        <div className="center">
+          <button
+            onClick={async () => {
+              await microsoftTeams.chat.openChat({
+                user: "AdeleV@6plbfs.onmicrosoft.com",
+                message: "Hi Adele! I am Ava Vellozzi, the new Explore intern on the Taos VSR Platform Team.",
+              });
+            }}
+          >
+            Start Chat
+          </button>
+          <button
+            onClick={async () =>
+              await microsoftTeams.chat.openGroupChat({
+                users: [
+                  "AdeleV@6plbfs.onmicrosoft.com",
+                  "AlexW@6plbfs.onmicrosoft.com",
+                ],
+                message:
+                  "Hi Adele and Alex! I am Ava Vellozzi, the new Explore intern on the Taos VSR Platform Team. Nice to meet you both!",
+                topic: "Group Chat",
+              })
+            }
+          >
+            Start Group Chat
+          </button>
+        </div>
+
+        <div className="tabList">
+          <TabList selectedValue={selectedValue} onTabSelect={onTabSelect}>
+            <Tab id="Local" value="local">
+              1. Build your app locally
+            </Tab>
+            <Tab id="Azure" value="azure">
+              2. Provision and Deploy to the Cloud
+            </Tab>
+            <Tab id="Publish" value="publish">
+              3. Publish to Teams
+            </Tab>
+          </TabList>
+          <div>
+            {selectedValue === "local" && (
+              <div>
+                <EditCode showFunction={showFunction} />
+                <CurrentUser userName={userName} />
+                <Graph />
+                {showFunction && <AzureFunctions />}
+              </div>
+            )}
+            {selectedValue === "azure" && (
+              <div>
+                <Deploy />
+              </div>
+            )}
+            {selectedValue === "publish" && (
+              <div>
+                <Publish />
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
